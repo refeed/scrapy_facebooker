8a9a7ea28a9f9688ae55f251b7dba34b5ff573e6
Rafid Aslam <rafidteam@gmail.com>
2017-08-06 22:33:39 +0700

    faceblib.graph: get_all_data_from_graph_api() fetch from "next" url
    Change `faceblib.graph.get_all_data_from_graph_api()` to be able to
    fetch and extract data when there's no 'cursors' in 'paging' by
    using 'next' graph API url that's found in 'paging'.
    This case was found in `{username}/posts` object in Graph API
    v2.7.

--- scrapy_facebooker/faceblib/graph.py
+++ scrapy_facebooker/faceblib/graph.py
@@ -1,5 +1,7 @@
 """This module contains helper functions for `facebook` library"""
 
+from urllib.parse import urlparse, parse_qs
+
 
 def get_all_data_from_graph_api(fgraph, fb_obj, **kwargs):
     """
@@ -15,10 +17,19 @@ def get_all_data_from_graph_api(fgraph, fb_obj, **kwargs):
     data_list.extend(graph_resp['data'])
 
     while 'paging' in graph_resp and 'next' in graph_resp['paging']:
-        graph_resp = fgraph.get_object(
-            fb_obj,
-            after=graph_resp['paging']['cursors']['after'],
-            **kwargs)
+        if 'cursors' in graph_resp['paging']:
+            graph_resp = fgraph.get_object(
+                fb_obj,
+                after=graph_resp['paging']['cursors']['after'],
+                **kwargs)
+        else:
+            # Extract parameters from the next paging graph API url
+            queries = parse_qs(urlparse(graph_resp['paging']['next']).query)
+            kwargs.update(queries)
+            graph_resp = fgraph.get_object(
+                fb_obj,
+                **kwargs
+            )
         data_list.extend(graph_resp['data'])
 
     return data_list
--- tests/faceblib/test_graph.py
+++ tests/faceblib/test_graph.py
@@ -18,6 +18,15 @@ with open(get_testfile_path('apiresp.json')) as ar:
         apiresp = bytes(ar.read(), 'utf-8')
         next_cursor_apiresp = bytes(next_ar.read(), 'utf-8')
 
+with open(get_testfile_path('post_api_resp_with_no_cursor.json')) as arnc:
+    post_api_resp_no_cursor = bytes(arnc.read(), 'utf-8')
+
+with open(get_testfile_path('next_post_api_resp_with_no_cursor.json')) as nrnc:
+    next_post_api_resp_no_cursor = bytes(nrnc.read(), 'utf-8')
+
+with open(get_testfile_path('blank_data_resp.json')) as br:
+    blank_data_resp = bytes(br.read(), 'utf-8')
+
 
 class FaceblibGraphTest(unittest.TestCase):
 
@@ -42,3 +51,42 @@ class FaceblibGraphTest(unittest.TestCase):
                  {'created_time': '2016-06-10T12:47:36+0000', 'id': '3'},
                  {'created_time': '2016-07-10T12:47:36+0000', 'id': '4'},
                  {'created_time': '2016-08-10T12:47:36+0000', 'id': '5'}])
+
+    def test_get_all_data_from_graph_api_with_no_cursor(self):
+        # Test get_all_data_from_graph_api() with no cursor, but there's
+        # a 'next' graph api url that provided by graph API.
+        graph = facebook.GraphAPI(
+            access_token='testAccessToken', version='2.7')
+
+        with requests_mock.Mocker() as m:
+            # Mock the Graph API responses.
+            m.get('https://graph.facebook.com/v2.7/testusername/posts'
+                  '?access_token=testAccessToken',
+                  content=post_api_resp_no_cursor,
+                  headers={'content-type': 'json'})
+            m.get('https://graph.facebook.com/v2.7/testusername/posts?limit=25'
+                  '&format=json&__paging_token=enc_samplePagingToken&until=133'
+                  '0763197&access_token=testAccessToken',
+                  content=next_post_api_resp_no_cursor,
+                  headers={'content-type': 'json'})
+            m.get('https://graph.facebook.com/v2.7/testusername/posts?limit=25'
+                  '&format=json&__paging_token=enc_samplePagingTokenB&until=13'
+                  '30763199&access_token=testAccessToken',
+                  content=blank_data_resp,
+                  headers={'content-type': 'json'})
+
+            result = get_all_data_from_graph_api(graph, 'testusername/posts')
+            self.assertEqual(
+                result,
+                [{'created_time': '2016-06-10T12:47:36+0000',
+                  'id': '1', 'story': 'A story'},
+                 {'created_time': '2016-07-10T12:47:36+0000',
+                  'id': '2', 'story': 'of'},
+                 {'created_time': '2016-08-10T12:47:36+0000',
+                  'id': '3', 'story': 'Test Username added a link'},
+                 {'created_time': '2016-06-10T12:47:36+0000',
+                  'id': '4', 'story': 'Scrapy'},
+                 {'created_time': '2016-07-10T12:47:36+0000',
+                  'id': '5', 'story': 'Facebooker'},
+                 {'created_time': '2016-08-10T12:47:36+0000',
+                  'id': '6', 'story': 'Graph'}])
